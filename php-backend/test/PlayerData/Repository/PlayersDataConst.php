e596916b22421b35e78edd8daca828656fd2a994
Rafael Melo Cardoso <xinhus@gmail.com>
2021-08-13 19:28:03 -0300

    Player Data Repository with filters and sorters

---
+++ php-backend/test/PlayerData/Repository/PlayersDataConst.php
@@ -0,0 +1,115 @@
+<?php
+
+
+namespace Test\TheScore\PlayerData\Repository;
+
+
+class PlayersDataConst
+{
+
+    public const ShaunHill = [
+        'Player' => 'Shaun Hill',
+        'Team' => 'MIN',
+        'Pos' => 'QB',
+        'Att' => 5,
+        'Att/G' => 1.7,
+        'Yds' => 5,
+        'Avg' => 1,
+        'Yds/G' => 1.7,
+        'TD' => 0,
+        'Lng' => '9',
+        '1st' => 0,
+        '1st%' => 0,
+        '20+' => 0,
+        '40+' => 0,
+        'FUM' => 0,
+    ];
+
+    public const JoeBanyard = [
+        'Player' => 'Joe Banyard',
+        'Team' => 'JAX',
+        'Pos' => 'RB',
+        'Att' => 2,
+        'Att/G' => 2,
+        'Yds' => 7,
+        'Avg' => 3.5,
+        'Yds/G' => 7,
+        'TD' => 0,
+        'Lng' => '7',
+        '1st' => 0,
+        '1st%' => 0,
+        '20+' => 0,
+        '40+' => 0,
+        'FUM' => 0,
+    ];
+
+    public const LanceDunbar = [
+        'Player' => 'Lance Dunbar',
+        'Team' => 'DAL',
+        'Pos' => 'RB',
+        'Att' => 9,
+        'Att/G' => 0.7,
+        'Yds' => 31,
+        'Avg' => 3.4,
+        'Yds/G' => 2.4,
+        'TD' => 1,
+        'Lng' => '10',
+        '1st' => 3,
+        '1st%' => 33.3,
+        '20+' => 0,
+        '40+' => 0,
+        'FUM' => 0,
+    ];
+
+    public const MarkIngram = [
+        'Player' => 'Mark Ingram',
+        'Team' => 'NO',
+        'Pos' => 'RB',
+        'Att' => 205,
+        'Att/G' => 12.8,
+        'Yds' => '1,043',
+        'Avg' => 5.1,
+        'Yds/G' => 65.2,
+        'TD' => 6,
+        'Lng' => '75T',
+        '1st' => 49,
+        '1st%' => 23.9,
+        '20+' => 4,
+        '40+' => 2,
+        'FUM' => 2,
+    ];
+    public const MarkIngram74 = [
+        'Player' => 'Mark Ingram',
+        'Team' => 'NO',
+        'Pos' => 'RB',
+        'Att' => 205,
+        'Att/G' => 12.8,
+        'Yds' => '1,043',
+        'Avg' => 5.1,
+        'Yds/G' => 65.2,
+        'TD' => 6,
+        'Lng' => 74,
+        '1st' => 49,
+        '1st%' => 23.9,
+        '20+' => 4,
+        '40+' => 2,
+        'FUM' => 2,
+    ];
+    public const MarkIngram76 = [
+        'Player' => 'Mark Ingram',
+        'Team' => 'NO',
+        'Pos' => 'RB',
+        'Att' => 205,
+        'Att/G' => 12.8,
+        'Yds' => '1,043',
+        'Avg' => 5.1,
+        'Yds/G' => 65.2,
+        'TD' => 6,
+        'Lng' => 76,
+        '1st' => 49,
+        '1st%' => 23.9,
+        '20+' => 4,
+        '40+' => 2,
+        'FUM' => 2,
+    ];
+}
